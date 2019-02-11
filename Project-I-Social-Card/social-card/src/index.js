c2c76c07ae287198efbadc30ba7226ae4451c89a
Francis Chen <fncis.chen@gmail.com>
2019-02-11 15:07:52 -0800

    refactored all elements that I needed to render as one element

--- Project-I-Social-Card/social-card/src/index.js
+++ Project-I-Social-Card/social-card/src/index.js
@@ -13,8 +13,6 @@ const HeaderInfo = {
     content: "Let's learn React by building simple interfaces with components." 
 }
 
-ReactDOM.render(<HeaderContainer headerInfo={HeaderInfo} />, document.getElementById("root"))
-
 const CardInfo = {
     bannerImage: <img src="https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png"></img>,
     cardContent: 
@@ -23,4 +21,10 @@ const CardInfo = {
     </div>
 }
 
-ReactDOM.render(<CardContainer cardInfo={CardInfo} />, document.getElementById("root"));
+const Element = (
+    <div>
+    <HeaderContainer headerInfo={HeaderInfo} /> <CardContainer cardInfo={CardInfo} />
+    </div>
+)
+
+ReactDOM.render(Element, document.getElementById("root"));
