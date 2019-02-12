211bdcf8851deecdd5ef062d778c009ae6f2ae30
Francis Chen <fncis.chen@gmail.com>
2019-02-12 13:30:03 -0800

    updated calculator js

---
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/CalculatorButtons.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Button.css';
+import NumberButton from "./NumberButton";
+
+class CalculatorButtons extends React.Component {
+    
+    render() {
+        const listOfButtons = this.props.buttons;
+    }
+}
--- Project-II-Calculator/calculator/src/index.js
+++ Project-II-Calculator/calculator/src/index.js
@@ -6,38 +6,53 @@ import App from './App';
 import CalculatorDisplay from "../src/components/DisplayComponents/CalculatorDisplay";
 import ActionButton from "../src/components/ButtonComponents/ActionButton";
 import NumberButton from "../src/components/ButtonComponents/NumberButton";
+import CalculatorButtons from "../src/components/ButtonComponents/CalculatorButtons";
 
-const numbers = [1,2,3,4,5,6,7,8];
+class Calculator extends React.Component {
 
-const element = (
+    render() {
+        return(
 
-    <div className="calculator">
+            <div className="calculator">
 
-        <div className="top">
-            <CalculatorDisplay />
-        </div>
+                <div className="top">
 
-        <div className="bottom">
+                     <CalculatorDisplay inputText={$this.props.calculator.inputText} />
+
+                </div>
+
+                <div className="bottom">
             
-            <div className ="left-side">
+                    <div className ="left-side">
             
-                <ActionButton text={$this.props.clearButton} />
+                        <ActionButton text={$this.props.calculator.clearButton} />
 
+                        <CalculatorButtons buttons={$this.props.calculator.buttons} />
 
-                <ActionButton text={$this.props.zeroButton} />
+                        <ActionButton text={$this.props.calculator.zeroButton} />
             
+                     </div>
+
+                    <div className ="rigth-side">
+            
+            
+            
+                    </div>
+
+                </div>
+
             </div>
 
-            <div className ="rigth-side">
-            
-            
-            
-            </div>
-
-        </div>
-
-    </div>
-);
+        );
+    }
+};
 
 
-ReactDOM.render(element, document.getElementById('root'));
+const calculator_data = {
+    numbers: [1,2,3,4,5,6,7,8],
+    clearButton: "clear",
+    zeroButton: 0,
+    operations: ["÷","×","+","-"],
+}
+
+ReactDOM.render(<Calculator calculator={calculator_data}/>, document.getElementById('root'));
