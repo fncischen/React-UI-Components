68f6f0fadacd4751c2e606a383d89f5cfbe18a2a
Francis Chen <fncis.chen@gmail.com>
2019-02-12 13:02:27 -0800

    updated calculator js

--- Project-II-Calculator/calculator/src/index.js
+++ Project-II-Calculator/calculator/src/index.js
@@ -3,4 +3,41 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import CalculatorDisplay from "../src/components/DisplayComponents/CalculatorDisplay";
+import ActionButton from "../src/components/ButtonComponents/ActionButton";
+import NumberButton from "../src/components/ButtonComponents/NumberButton";
+
+const numbers = [1,2,3,4,5,6,7,8];
+
+const element = (
+
+    <div className="calculator">
+
+        <div className="top">
+            <CalculatorDisplay />
+        </div>
+
+        <div className="bottom">
+            
+            <div className ="left-side">
+            
+                <ActionButton text={$this.props.clearButton} />
+
+
+                <ActionButton text={$this.props.zeroButton} />
+            
+            </div>
+
+            <div className ="rigth-side">
+            
+            
+            
+            </div>
+
+        </div>
+
+    </div>
+);
+
+
+ReactDOM.render(element, document.getElementById('root'));
